2cbf565d9e11e66f89a8ead7e3aec5546193b4b4
Gurmanpreet Kaur <gkaur286@my.centennialcollege.ca>
2017-08-10 10:57:00 -0400

    Created a public property as an Alias to Program.Calculator

--- COMP-123-LESSON-2-Calculator/SplashForm.cs
+++ COMP-123-LESSON-2-Calculator/SplashForm.cs
@@ -12,12 +12,27 @@ using System.Windows.Forms;
  * student number - 300933392
  * date - 8 august , 2017
  * description - this is the SplashForm class  
- * version 0.3-  Refactored the SplashForm "Tick" handler
+ * version 0.4-  Created a public property as an Alias to Program.Calculator
  * */
 namespace COMP_123_LESSON_2_Calculator
 {
     public partial class SplashForm : Form
     {
+        // private instance variables 
+        private Calculator _calculator;
+
+        //public properties
+        public Calculator Calculator
+        {
+            get
+            {
+                return Program.calculator;
+            }
+        }
+            // constructors ----------------------------
+           /// <summary>
+           /// /this is the main constructor for the splashform 
+           /// </summary>
         public SplashForm()
         {
             InitializeComponent();
@@ -30,7 +45,7 @@ namespace COMP_123_LESSON_2_Calculator
         private void SplashFormTimer_Tick(object sender, EventArgs e)
         {
             
-            Program.calculator.Show();
+            this.Calculator+.Show();
             this.Hide();
 
             SplashFormTimer.Enabled = false; //turn timer off
